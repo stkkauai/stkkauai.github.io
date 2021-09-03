553293bd65d8f5627d6b9f7a9d2cb3177376e67f
stkkauai <sustainabletourismkauaiweb@gmail.com>
2021-09-03 12:29:25 -1000

    Add Images

--- allpages.js
+++ allpages.js
@@ -4,7 +4,7 @@ window.onscroll = function() {scrollFunction()};
       if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
         document.getElementById("navbar").style.top = "0";
       } else {
-        document.getElementById("navbar").style.top = "-50px";
+        document.getElementById("navbar").style.top = "-60px";
       }
     }
 
@@ -19,4 +19,29 @@ window.onscroll = function() {scrollFunction()};
                 'left': newPos
             });
         });
+    });
+
+    $(document).ready(function(){
+      // Add smooth scrolling to all links
+      $("a").on('click', function(event) {
+    
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+          // Prevent default anchor click behavior
+          event.preventDefault();
+    
+          // Store hash
+          var hash = this.hash;
+    
+          // Using jQuery's animate() method to add smooth page scroll
+          // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+          $('html, body').animate({
+            scrollTop: $(hash).offset().top
+          }, 800, function(){
+    
+            // Add hash (#) to URL when done scrolling (default click behavior)
+            window.location.hash = hash;
+          });
+        } // End if
+      });
     });
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 window.addEventListener("scroll", function() {
     const distance = window.scrollY;
     document.querySelector("header").style.transform = `translateY(${distance *
-      0.4}px)`;
+      0.6}px)`;
     document.querySelector(
       ".heroheading"
     ).style.transform = `translateY(${distance * 0.001}px)`;
