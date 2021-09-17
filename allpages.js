f01aee68c548225f3a5c099122d8912de8b25d82
stkkauai <sustainabletourismkauaiweb@gmail.com>
2021-09-17 12:14:34 -1000

    Hero Fixed

--- allpages.js
+++ allpages.js
@@ -4,7 +4,7 @@ window.onscroll = function() {scrollFunction()};
       if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
         document.getElementById("navbar").style.top = "0";
       } else {
-        document.getElementById("navbar").style.top = "-60px";
+        document.getElementById("navbar").style.top = "-70px";
       }
     }
 
